600234d4066fdbaf3041b197f7234d7eb9aef6ab
mayfak <konstantinmokrousov@MacBook-Air-Konstantin.local>
2025-02-14 16:08:15 +0700

    auto

--- run2.sh
+++ run2.sh
@@ -1,3 +1,19 @@
 #!/bin/sh
 
-taskset --cpu-list 2 ./binary_search_3 $1 
+taskset --cpu-list 2 ./binary_search_3 512 
+taskset --cpu-list 2 ./binary_search_3 1024 
+taskset --cpu-list 2 ./binary_search_3 2048 
+taskset --cpu-list 2 ./binary_search_3 4096 
+taskset --cpu-list 2 ./binary_search_3 8192 
+taskset --cpu-list 2 ./binary_search_3 16384
+taskset --cpu-list 2 ./binary_search_3 32768
+taskset --cpu-list 2 ./binary_search_3 65536
+taskset --cpu-list 2 ./binary_search_3 131072
+taskset --cpu-list 2 ./binary_search_3 262144 
+taskset --cpu-list 2 ./binary_search_3 524288 
+taskset --cpu-list 2 ./binary_search_3 1048576 
+taskset --cpu-list 2 ./binary_search_3 2097152 
+taskset --cpu-list 2 ./binary_search_3 4194304
+taskset --cpu-list 2 ./binary_search_3 8388608
+taskset --cpu-list 2 ./binary_search_3 16777216
+
